b21a150eaf38853fda29d21b33c4ccf86c6c4809
shivangsrivastav-gog <shivang.srivastav@geeksofgurukul.com>
2025-09-24 07:10:16 +0530

    Update metaverse.sol

--- Contracts/metaverse.sol
+++ Contracts/metaverse.sol
@@ -26,8 +26,7 @@ contract Project {
         string memory _description,
         string memory _metadataURI
     ) public returns (uint256) {
-        uint256 assetId = _nextAssetId;
-        _nextAssetId++;
+        uint256 assetId = _nextAssetId++;
         
         assets[assetId] = MetaverseAsset({
             id: assetId,
@@ -52,72 +51,4 @@ contract Project {
         require(_to != msg.sender, "Cannot transfer to yourself");
         
         address previousOwner = assets[_assetId].owner;
-        assets[_assetId].owner = _to;
-        assets[_assetId].isListed = false; // Remove from listing when transferred
-        
-        // Update owner arrays
-        _removeAssetFromOwner(previousOwner, _assetId);
-        ownerAssets[_to].push(_assetId);
-        
-        emit AssetTransferred(_assetId, previousOwner, _to);
-    }
-    
-    // Core Function 3: List Asset for Sale
-    function listAssetForSale(uint256 _assetId, uint256 _price) public {
-        require(assets[_assetId].owner == msg.sender, "Not the asset owner");
-        require(_price > 0, "Price must be greater than 0");
-        
-        assets[_assetId].price = _price;
-        assets[_assetId].isListed = true;
-        
-        emit AssetListed(_assetId, _price);
-    }
-
-    // Core Function 4: Buy Asset
-    function buyAsset(uint256 _assetId) public payable {
-        MetaverseAsset storage asset = assets[_assetId];
-        require(asset.isListed, "Asset is not listed for sale");
-        require(msg.value >= asset.price, "Insufficient payment");
-        require(msg.sender != asset.owner, "Owner cannot buy their own asset");
-
-        address previousOwner = asset.owner;
-        uint256 salePrice = asset.price;
-
-        // Update ownership
-        asset.owner = msg.sender;
-        asset.isListed = false;
-        asset.price = 0; // Reset price after purchase
-
-        // Update ownerAssets mappings
-        _removeAssetFromOwner(previousOwner, _assetId);
-        ownerAssets[msg.sender].push(_assetId);
-
-        // Transfer funds to seller
-        payable(previousOwner).transfer(salePrice);
-
-        emit AssetTransferred(_assetId, previousOwner, msg.sender);
-    }
-    
-    // Additional helper functions
-    function getAsset(uint256 _assetId) public view returns (MetaverseAsset memory) {
-        return assets[_assetId];
-    }
-    
-    function getOwnerAssets(address _owner) public view returns (uint256[] memory) {
-        return ownerAssets[_owner];
-    }
-    
-    function getAllListedAssets() public view returns (uint256[] memory) {
-        uint256[] memory listedAssets = new uint256[](_nextAssetId - 1);
-        uint256 counter = 0;
-        
-        for (uint256 i = 1; i < _nextAssetId; i++) {
-            if (assets[i].isListed) {
-                listedAssets[counter] = i;
-                counter++;
-            }
-        }
-        
-        // Resize array to actual count
-        uint256[] memory result = new uint256[](counter);
-        for (uint256 i = 0; i < count
+        as
